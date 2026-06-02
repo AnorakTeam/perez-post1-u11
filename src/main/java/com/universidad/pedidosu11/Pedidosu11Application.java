5b2a7e30d8e24939511bff5599ba8c8124ac7a52
José Manuel Pérez Rodríguez <anorakteam@gmail.com>
2026-06-02 13:03:45 -0500

    feat: setup inicial del proyecto

---
+++ src/main/java/com/universidad/pedidosu11/Pedidosu11Application.java
@@ -0,0 +1,13 @@
+package com.universidad.pedidosu11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pedidosu11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pedidosu11Application.class, args);
+	}
+
+}
